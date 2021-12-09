d0466b2ec3839d35e24dbe863c4452f3d56a3f50
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2021-12-09 18:45:55 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+var profitLossInput=document.querySelectorAll('.input-price');
+var btnInput=document.querySelector("#input-btn");
+var output=document.querySelector("#output");
+
+function clickHandler()
+{
+    var currentPrice=Number(profitLossInput[2].value);
+    var initialPrice=Number(profitLossInput[0].value);
+    var stockquantity=Number(profitLossInput[1].value);
+    if(currentPrice>initialPrice)
+    {
+        var profit=(currentPrice-initialPrice) * stockquantity;
+        var profitpercentage=(profit/initialPrice) *100;
+        output.innerText="Hey!! the profit is " + profit + " and the percentage is "
+        + profitpercentage + "%";
+
+    }
+    else if(initialPrice>currentPrice)
+    {
+        var loss=(initialPrice-currentPrice) * stockquantity;
+        var losspercentage=(loss/initialPrice) * 100;
+        output.innerText="Hey!! the loss is " + loss + " and the percentage is "
+        + losspercentage + "%";
+       
+    }
+    else{
+        output.innerText="You faced no loss no profit!!";
+
+    }
+
+}
+
+btnInput.addEventListener("click",clickHandler);
